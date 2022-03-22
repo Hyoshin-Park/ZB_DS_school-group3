b39c4bff415f733083e75f2eb3b5627fe3e64b2b
mjcode1588 <98296098+mjcode1588@users.noreply.github.com>
2022-03-22 12:06:48 +0900

    Add files via upload

---
+++ 스터디 코드/22_03_22/124나라의숫자.py
@@ -0,0 +1,9 @@
+url = "https://programmers.co.kr/learn/courses/30/lessons/12899"
+
+def solution(n):
+    if n<=3:
+        return "124"[n-1]
+    else:
+        q, r = divmod(n-1, 3)
+        return solution(q) + "124"[r]
+
